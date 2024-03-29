f94253262f99c506691fe7d045ed5f5e7d744124
Henok <153111713+hnk36@users.noreply.github.com>
2024-03-29 22:57:41 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,121 +1,196 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
-
-int main(){
-    /*my project is simple bank management system */
-    string name,password;
-    string name_store={"0"};
-    int account;
-    double balance,deposit,withDraw;
-    int BigBalance[1]={0};
-    int Big_deposit[1]={0};
-    int store=0;
-    string store1={"0"};
-    home:
-    cout<<"---BANK MANAGEMENT SYSTEM-----  "<<endl;
-    cout<<"1)Add Account"<<endl;
-    cout<<"2)Deposit"<<endl;
-    cout<<"3)Withdraw"<<endl;
-    cout<<"4)Search Account"<<endl;
-    cout<<"5)Exit"<<endl;
-    char choice;
-    cout<<"Enter your choice: ";
-    cin>>choice;
-    switch(choice){
-        case '1':
-            add:
-            cout<<"Enter your account number:";
-            cin>>account;
-            cout << "Enter your password:";
-            cin >> password;
-            store1=password;
-            if(account!=store) {
-                if(store1==password) {
-                    store = account;
-                    cout << "Enter your name:";
-                    cin >> name;
-                    name_store=name;
-                    cout << "Enter your balance:";
-                    cin >> balance;
-                    cout << "add account successfully" << endl;
-                    BigBalance[1] = balance;
-                }
-                else {
-                    cout << "wrong password" << endl;
-                    goto add;
-                }
-            }
-            else{
-                cout << "account already exist";
-            }
-            goto home;
-            case '2':
-                deposit:
-                cout<<"Enter your account number:";
-                cin>>account;
-                if(store==account) {
-                    cout << "Enter your password:";
-                    cin >> password;
-                    if(password==store1) {
-                        cout << "Enter your name:";
-                        cin >> name;
-                        cout << "Enter you want to deposit money amount: ";
-                        cin >> deposit;
-                        cout << "The deposit successfully completed and you was deposit" << deposit << endl;
-                        Big_deposit[1] = deposit + BigBalance[1];
-                        cout << "your total balance is " << Big_deposit[1] << endl;
-                    }
-                    else {
-                        cout << "wrong password" << endl;
-                        goto deposit;
-                    }
-                }
-                else{
-                    cout<<"account not found"<<endl;
-                }
-                     goto home;
-                    case '3':
-                        withdraw:
-                        cout<<"Enter your account number:";
-            cin>>account;
-            cout << "Enter your name:";
-            cin >> name;
-            cout << "Enter your password:";
-            cin >> password;
-            if(store==account&&store1==password) {
-                cout<<"Enter amount of  you want withdraw :";
-                cin>>withDraw;
-                if(withDraw<Big_deposit[1]) {
-                    cout << "withdraw is" << deposit;
-                    Big_deposit[1]-=withDraw;
-                    cout << "your total balance is " << Big_deposit[1]<<endl;
-                }
-            }
-            else{
-                cout<<"account not found or your input password is invalid"<<endl;
-                goto withdraw;
-            }
-                   goto home;
-        case'4':
-            search:
-            cout<<"Enter account number: ";
-            cin>>account;
-            if(store==account){
-                cout<<"Name: "<<name_store<<endl;
-                cout<<"Balance: "<<Big_deposit[1]<<endl;
-                cout<<"account number: "<<account<<endl;
-            }
-      else{
-         cout<<"account not found try to add Account"<<endl;
-         goto search;
-           }
-      goto home;
-      case'5':
-          cout<<"Thank you for using our bank programming: ";
-          break;
-          default:
-            cout<<"Wrong choice please try again "<<endl;
+int main() {
+    for(int i=0;i<90;i++) {
+        cout<<"*";
     }
+    cout<<endl;
+    cout<<"                                     CALCULATOR"<<endl;
+    for(int i=0;i<90;i++) {
+        cout << "*";
+    }
+    cout<<endl;
+    cout<<"1:Addition"<<endl;
+    cout<<"2:Subtraction"<<endl;
+    cout<<"3:Multiplication"<<endl;
+    cout<<"4:Division"<<endl;
+    cout<<"5:Square"<<endl;
+    cout<<"6:Square Root"<<endl;
+    cout<<"7:Modulus"<<endl;
+    cout<<"8:Power"<<endl;
+    cout<<"9:sine"<<endl;
+    cout<<"10:Cosine"<<endl;
+    cout<<"11:log"<<endl;
+    cout<<"12:Tangent"<<endl;
+    cout<<"13:Cotangent"<<endl;
+    cout<<"14:inverse Sine"<<endl;
+    cout<<"15:inverse Cosine"<<endl;
+    char choice;
+    choose:
+    {
+        cout << "Enter your choice: ";
+        cin >> choice;
+        switch (choice) {
+            case '1':
+                // Declare variables
+                cout << "Addition" << endl;
+                int num1, num2, num3, sum;
+                //Prompt the user to enter two numbers
+                cout << "Enter the first number: ";
+                cin >> num1;
+                cout << "Enter the second number: ";
+                cin >> num2;
+                //Calculate the sum
+                sum = num1 + num2;
+                cout << sum << endl;
+                goto choose;
+            case '2':
+                cout << "Subtraction" << endl;
+                int sub;
+                cout << "Enter the first number: ";
+                cin >> num1;
+                cout << "Enter the thr second number: ";
+                cin >> num2;
+                //calculate the different
+                sub = num1 - num2;
+                cout << sub << endl;
+                goto choose;
+            case '3':
+                int mul;
+                cout << "Multiplication" << endl;
+                cout << "Enter the first number: ";
+                cin >> num1;
+                cout << "Enter the second number: ";
+                cin >> num2;
+                //calculate the Multiplication;
+                mul = num1 * num2;
+                cout << mul << endl;
+                goto choose;
+            case '4':
+                cout << "Division" << endl;
+                int div;
+                cout << "Enter the first number: ";
+                cin >> num1;
+                cout << "Enter the second number: ";
+                cin >> num2;
+                //calculate the division
+                div = num1 / num2;
+                cout << div;
+                goto choose;
+            case '5':
+                cout << "Square" << endl;
+                int sq;
+                cout << "Enter the number: ";
+                cin >> num1;
+                //calculate the square
+                sq = num1 * num1;
+                cout << sq;
+                goto choose;
+            case '6':
+                cout << "Square Root" << endl;
+                float sqr;
+                cout << "Enter the number: ";
+                cin >> num3;
+                //calculate the square root
+                sqr = sqrt(num3);
+                cout << sqr << endl;
+                goto choose;
+            case '7':
+                cout << "Modulus";
+                int mod;
+                cout << "Enter the first number: ";
+                cin >> num1;
+                cout << "Enter the second number:";
+                cin >> num2;
+                mod = num1 % num2;
+                cout << mod << endl;
+                goto choose;
+            case '8':
+                int result;
+                cout << "Power ";
+                cout << "Enter the first number: ";
+                cin >> num1;
+                cout << "Enter the second number: ";
+                cin >> num2;
+                //calculate the power
+                result = pow(num1, num2);
+                cout << result << endl;
+                goto choose;
+            case '9':
+
+                cout << "Sine";
+                double result1, angle;
+
+                cout << "Enter the angle:";
+                cin >> angle;
+                //convert angle to a double
+                angle = double(angle);
+                //calculate the sine
+                result1 = sin(angle * M_PI / 180); // convert angle to radians
+                cout << result1 << endl;
+                goto choose;
+            case '10':
+                cout << "Cosine";
+                double result2, angle2;
+                cout << "Enter the angle in degrees: ";
+                cin >> angle2;
+                result2 = cos(angle2 * M_PI / 180); // convert angle to radians before calculating cosine
+                cout << "Cosine of " << angle2 << " degrees is: " << result2 << endl;
+                break;
+            case '11':
+                cout << "Logarithm";
+                double num4, result3;
+                cout << "Enter a number: ";
+                cin >> num4;
+                result3 = log(num4); // natural logarithm
+                cout << "Natural logarithm of " << num4 << " is: " << result3 << endl;
+                goto choose;
+            case '12':
+                cout << "Tangent";
+                double result4, angle3;
+                cout << "Enter the angle in degrees: ";
+                cin >> angle3;
+                result4 = tan(angle3 * M_PI / 180); // convert angle to radians before calculating tangent
+                cout << "Tangent of " << angle3 << " degrees is: " << result4 << endl;
+                goto choose;
+            case '13':
+                cout << "Cotangent";
+                double result5, angle4;
+                cout << "Enter the angle in degrees: ";
+                cin >> angle4;
+                result5 = 1 / tan(angle4 * M_PI / 180); // cotangent is the reciprocal of tangent
+                cout << "Cotangent of " << angle4 << " degrees is: " << result5 << endl;
+                goto choose;
+            case '14':
+                cout << "Inverse Sine";
+                double result6, value;
+                cout << "Enter a value between -1 and 1: ";
+                cin >> value;
+                result6 = asin(value) * 180 / M_PI; // calculate inverse sine in degrees
+                cout << "Inverse Sine of " << value << " is: " << result6 << " degrees" << endl;
+            case '15':
+                cout << "Inverse Cosine";
+                double result7, value2;
+                cout << "Enter a value between -1 and 1: ";
+                cin >> value2;
+                result7 = acos(value2) * 180 / M_PI; // calculate inverse cosine in degrees
+                cout << "Inverse Cosine of " << value2 << " is: " << result7 << " degrees" << endl;
+
+                goto choose;
+            default:
+                cout<<"User input is not vlaid pleaase enter a choice value number between 1 and 15 ";
+                goto choose;
+
+
+
+
+
+
+        }
+
+    }
+
 
     return 0;
 }
